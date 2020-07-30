cd55c5d52284df24889d54db2b58263883bdae85
azocher <anna.zocher@gmail.com>
2020-07-29 23:13:29 -0700

    just capitalized Models because fuck it

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const cors = require('cors')
 const passport = require('passport')
 
 const app = express()
-const User = require("./models/User")
+const User = require("./Models/User")
 
 const uri = process.env.MONGOD_URI
 
